01f46ddf40cf274e354c7851de40f04d43afd3c5
Wissenss <wissenschaftecht@gmail.com>
2022-07-30 23:34:29 -0500

    added settings meni

--- script.py
+++ script.py
@@ -11,8 +11,8 @@ class Database():
 
         self.tasks_table()
 
-    def tasks_table(self):
-        self.cursor.execute("""CREATE TABLE IF NOT EXISTS tasks (
+    def tasks_table(self, name='tasks'):
+        self.cursor.execute(f"""CREATE TABLE IF NOT EXISTS {name} (
             id integer PRIMARY KEY,
             name text,
             status text);""")
@@ -26,8 +26,12 @@ class Database():
         self.cursor.execute(f"DELETE FROM tasks WHERE id={id}")
         self.connection.commit()
 
-    def get_records(self):
-        self.cursor.execute("""SELECT id,name, status FROM tasks""")
+    def get_records(self, name="tasks"):
+        self.cursor.execute(f"""SELECT id,name, status FROM {name}""")
+        return self.cursor.fetchall()
+
+    def get_tables(self):
+        self.cursor.execute("""SELECT name from sqlite_master where type='table'""")
         return self.cursor.fetchall()
 
     def update_status(self, id, newstatus):
@@ -108,6 +112,14 @@ class App():
         completed_button = StyledButton(root, **kwargs)
         completed_button.place(x=340, y=220, width=140, height=20)
 
+        #task list selector
+        self.tasks_list = self.database.get_tables()
+        #['default', 'second tasks', 'third tasks']
+        self.current_task_list = tk.StringVar(root)
+        self.current_task_list.set(self.tasks_list[0][0])
+        task_selector = tk.OptionMenu(root, self.current_task_list, *self.tasks_list)
+        task_selector.place(x=20, y=60, width=300, height=20)
+
         #task list
         #configure Treeview
         style = ttk.Style()
@@ -126,7 +138,7 @@ class App():
         self.task_tree.column("Task", width=300)
         self.task_tree.heading("Task", text="Tasks")
 
-        self.task_tree.place(x=20, y=60)
+        self.task_tree.place(x=20, y=80)
 
         #status tags
         self.task_tree.tag_configure('completed', background='#68a832')
@@ -135,11 +147,12 @@ class App():
 
         #load saved tasks from database
         self.task_id = 0
-        self.load_tasks()
+        self.load_tasks(self.current_task_list.get())
 
     #managment methods
-    def load_tasks(self):
-        tasks = self.database.get_records()
+    def load_tasks(self, name):
+        self.clear_task()
+        tasks = self.database.get_records(name)
         if tasks:
             self.task_id = tasks[-1][0]+1
             for task in tasks:
@@ -156,11 +169,10 @@ class App():
         self.task_tree.delete(task)
 
     def clear_task(self):
-        if messagebox.askyesno(title='Delete all', message='are you sure?'):
-            tasks = self.task_tree.get_children()
-            for task in tasks:
-                self.remove_task(task)
-            self.task_id=0
+        tasks = self.task_tree.get_children()
+        for task in tasks:
+            self.remove_task(task)
+        self.task_id=0
 
     #style methods
     def change_tag(self, iid, tag):
@@ -179,6 +191,10 @@ class App():
         selected_tasks = self.task_tree.selection()
         for task in selected_tasks:
             self.remove_task(task)
+
+    def clear_task_button(self):
+        if messagebox.askyesno(title='Delete all', message='are you sure?'):
+            self.clear_task()
 
     def add_task_button(self):
         #update task counter
@@ -208,6 +224,60 @@ class App():
         else:
             pass
 
-App()
+class Select_list_menu():
+    def __init__(self, main_window):
+        #save root database
+        self.database = main_window.database
+        self.main_window = main_window
+
+        #create menu window
+        self.menu = tk.Toplevel(root)
+
+        #config menu window
+        self.menu.title('Load List')
+        self.menu.geometry("250x300")
+
+        #dropdown tasks list
+        self.selected_list = tk.StringVar()
+        self.selected_list.set(main_window.current_task_list.get())
+        self.list = tk.OptionMenu(self.menu, self.selected_list, self.database.get_tables())
+        self.list.place(x=20, y=20, height=20, width=210)
+
+        #load button
+        load_button = tk.Button(self.menu, text="Load", command=self.load_button)
+        load_button.place(x=20, y=60, height=20, width=210)
+
+        #change name button
+        changeName_button = tk.Button(self.menu, text="Change Name", command=self.changeName_button)
+        changeName_button.place(x=20, y=100, height=20, width=210)
+
+        #create new list button
+        newList_button = tk.Button(self.menu, text="Create New", command=self.addList_button)
+        newList_button.place(x=20, y=140, height=20, width=210)
+
+        #remove list button
+        removeList_button = tk.Button(self.menu, text="Delete Current", command=self.removeList_button)
+        removeList_button.place(x=20, y=180, height=20, width=210)
+
+    def load_button(self):
+        self.main_windowk.load_tasks(self.selected_list.get())
+        self.delf()
+
+    def addList_button(self):
+        name = 'test new table'
+        self.database.tasks_table(name)
+
+    def changeName_button(self):
+        pass
+
+    def removeList_button(self):
+        pass
+
+    def __delf__(self):
+        pass
+
+main_window = App()
+Select_list_menu(main_window)
+
 
 root.mainloop()
